b2e1583c1a0053bde56ea58840287a56ea9b2f9d
AhiruNeko <AhiruNeko@outlook.com>
2025-08-14 11:57:45 +0800

    original stm32

---
+++ STM32Interface/STM32F103Interface/cmake-build-debug/CMakeFiles/STM32F103Interface.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d
@@ -0,0 +1,33 @@
+CMakeFiles/STM32F103Interface.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj: \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Src\stm32f1xx_hal_uart.c \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Core\Inc/stm32f1xx_hal_conf.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_rcc.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_def.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Device\ST\STM32F1xx\Include/stm32f1xx.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Device\ST\STM32F1xx\Include/stm32f103xb.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Include/core_cm3.h \
+ c:\progra~2\gnuarm~1\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\features.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\CMSIS\Device\ST\STM32F1xx\Include/system_stm32f1xx.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ c:\progra~2\gnuarm~1\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_rcc_ex.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_gpio.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_gpio_ex.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_exti.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_dma.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_dma_ex.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_cortex.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_flash.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_flash_ex.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_pwr.h \
+ D:\OKC\projects\codes\Others\DesktopPet\STM32Interface\STM32F103Interface\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_uart.h
